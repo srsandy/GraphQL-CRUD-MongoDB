869df8290960d9334ce5dcad985792c1ad89be08
srsandy <sandeepranjan2007@yahoo.co.in>
2018-06-14 00:59:33 +0530

    graphql-mongodb-CRUD

---
+++ src/resolvers.js
@@ -0,0 +1,23 @@
+import Product from './models/product';
+
+export const resolvers = {
+	Query: {
+		async allProducts() {
+			return await Product.find()
+		},
+		async getProduct(_, { _id }) {
+            return await Product.findById(_id);
+        }
+	},
+	Mutation: {
+		async creatProduct(_,{input}){
+			return await Product.create(input);
+		},
+        async updateProduct(_, { _id, input }) {
+            return await Product.findOneAndUpdate({ _id }, input, { new: true })
+        },
+        async deleteProduct(_, { _id }) {
+            return await Product.findByIdAndRemove(_id);
+        }
+	}
+}
